ab14fc62b275becba3ff9cb7982ba9cc229af0d8
miladrahimi <info@miladrahimi.com>
2019-09-17 15:54:25 +0430

    update redirection param and readme

--- README.md
+++ README.md
@@ -8,25 +8,52 @@ It is written in Go language and ready to deploy on [Heroku](https://heroku.com)
 After running the application, you can fetch a single url this way:
 
 ```
-[your-server]/?url=[url]
+[host]:[port]/?url=[url]
 ```
 
 For example:
 
 ```
-https://example.com/?https://target.com/video.mp4
+https://proxy.com/?https://url.com/video.mp4
 ```
 
-You may set referrer this way:
+The proxy pass the request body and headers (http method, referrer, auth, etc.) to the requested url.
+
+### Redirection
+
+The requested url might return a http redirection response.
+In this case the proxy behave based on the `redirection` parameter.
 
 ```
-[your-server]/?url=[url]&referrer=[another-url]
+[host]:[port]/?url=[url]&redirection=[value]
 ```
 
-And if you don't want the proxy to follow redirection:
+#### Default behaviour
+Without the `redirection` parameter the default behaviour is returning the response with no manipulation.
+For example, if the request was `http://proxy.com/?url=https://google.com`
+the response would be redirection to `https://www.google.com`.
+It might not be your expected result, because if you make this request on using your browser,
+it redirects you to the real Google address (`https://www.google.com`) with no proxy.
+
+#### follow
+With the `follow` value for the `redirection` parameter the behaviour is redirecting through the proxy.
+For example, if the request was `http://proxy.com/?url=https://google.com&redirection=follow`
+the response would be redirection to `http://proxy.com/?url=https://www.google.com&redirection=follow`.
+As you can see, if your client handle redirection it redirects you to the new url through the proxy.
+
+#### stop
+With the `stop` value for the `redirection` parameter the behaviour is returning a JSON response contains location.
+For example, if the request was `http://proxy.com/?url=https://google.com&redirection=stop`
+the response would be:
 
 ```
-[your-server]/?url=[url]&follow=false
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+Content-Length: ...
+Content-Type: application/json
+Date: ...
+
+{"location":"https://www.google.com/"}
 ```
 
 ## License
--- main.go
+++ main.go
@@ -19,67 +19,53 @@ const (
 )
 
 func main() {
-	// Retrieve proxy host
-	proxyHost := os.Getenv("HOST")
-	if proxyHost == "" {
-		proxyHost = DefaultHost
-	}
+	proxyUrl := host() + ":" + port()
 
-	// Retrieve proxy port
-	proxyPort := os.Getenv("PORT")
-	if proxyPort == "" {
-		proxyPort = DefaultPort
-	}
-
-	proxyUrl := proxyHost + ":" + proxyPort
-
-	// The favicon.ico
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		content, _ := ioutil.ReadFile("favicon.ico")
-		w.Header().Set("Content-Type", "image/x-icon")
-		_, _ = fmt.Fprintln(w, string(content))
+	http.HandleFunc("/favicon.ico", func(rw http.ResponseWriter, r *http.Request) {
+		favicon(rw)
 	})
 
-	// Proxy main route
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+	http.HandleFunc("/", func(rw http.ResponseWriter, request *http.Request) {
+		rw.Header().Set("Access-Control-Allow-Origin", "*")
 
-		query := r.URL.Query()
+		query := request.URL.Query()
 
 		if query.Get("url") == "" {
-			displayMessage(w, "Welcome!")
+			displayError(rw, "Nothing requested.")
 			return
 		}
 
 		target, err := url.Parse(query.Get("url"))
 		if err != nil || target.IsAbs() == false {
-			displayError(w, "The requested URL is invalid.")
+			displayError(rw, "The requested URL is invalid.")
 			return
 		}
 
 		// Make reverse proxy
 		proxy := &httputil.ReverseProxy{
-			Director: func(req *http.Request) {
+			Director: func(r *http.Request) {
 				if referrer := query.Get("referrer"); referrer != "" {
-					req.Header.Set("Referer", referrer)
+					r.Header.Set("Referer", referrer)
 				} else {
-					req.Header.Set("Referer", target.Scheme+"://"+target.Host+"/")
+					r.Header.Set("Referer", target.Scheme+"://"+target.Host+"/")
 				}
 
-				req.Header.Set("Origin", target.Host)
-				req.Host = target.Host
-				req.URL = target
+				r.Header.Set("Origin", target.Host)
+				r.Host = target.Host
+				r.URL = target
 
-				log.Println(req)
+				log.Println(r)
 			},
 
 			ModifyResponse: func(r *http.Response) error {
+				r.Header.Del("Access-Control-Allow-Origin")
+
 				// Handle redirection responses
-				if location := r.Header.Get("Location"); location != "" {
-					if query.Get("follow") != "false" {
-						r.Header.Set("Location", proxyUrl+"/?url="+r.Header.Get("Location"))
-					} else {
-						modifyResponseToRedirection(r, location)
+				if r.StatusCode >= 300 && r.StatusCode < 400 && r.Header.Get("Location") != "" {
+					if query.Get("redirection") == "follow" {
+						r.Header.Set("Location", proxyUrl+"/?redirection=follow&url="+r.Header.Get("Location"))
+					} else if query.Get("redirection") == "stop" {
+						modifyResponseToRedirection(r, r.Header.Get("Location"))
 					}
 				}
 
@@ -87,47 +73,65 @@ func main() {
 			},
 		}
 
-		proxy.ServeHTTP(w, r)
+		proxy.ServeHTTP(rw, request)
 	})
 
-	log.Println("Started: " + proxyUrl)
-	log.Fatal(http.ListenAndServe(":"+proxyPort, nil))
+	log.Println("Serving on " + proxyUrl)
+	log.Fatal(http.ListenAndServe(":"+port(), nil))
 }
 
-// Create an error response
-func displayError(w http.ResponseWriter, message string) {
-	w.WriteHeader(http.StatusBadRequest)
-	w.Header().Set("Content-type", "text/plain")
+// host will return the proxy host
+func host() string {
+	if host := os.Getenv("HOST"); host == "" {
+		return DefaultHost
+	} else {
+		return host
+	}
+}
 
-	_, err := w.Write([]byte("Error: " + message))
+// port will return the proxy port
+func port() string {
+	if port := os.Getenv("PORT"); port == "" {
+		return DefaultPort
+	} else {
+		return port
+	}
+}
+
+// favicon will return the favicon icon
+func favicon(rw http.ResponseWriter) {
+	content, _ := ioutil.ReadFile("favicon.ico")
+	rw.Header().Set("Content-Type", "image/x-icon")
+	_, _ = fmt.Fprintln(rw, string(content))
+}
+
+// displayError will create an error response
+func displayError(rw http.ResponseWriter, error string) {
+	rw.WriteHeader(http.StatusBadRequest)
+	rw.Header().Set("Content-type", "application/json")
+
+	body, err := json.Marshal(map[string]string{"error": error})
+	if err != nil {
+		panic("Cannot generate the JSON.")
+	}
+
+	_, err = rw.Write(body)
 	if err != nil {
 		panic("Cannot respond to the request.")
 	}
 }
 
-// Create an error response
-func displayMessage(w http.ResponseWriter, message string) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-type", "text/plain")
-
-	_, err := w.Write([]byte(message))
-	if err != nil {
-		panic("Cannot respond to the request.")
-	}
-}
-
-// Modify response to location
-func modifyResponseToRedirection(response *http.Response, location string) {
+// modifyResponseToRedirection will modify the response to a location response
+func modifyResponseToRedirection(r *http.Response, location string) {
 	body, err := json.Marshal(map[string]string{"location": location})
 	if err != nil {
 		panic("Cannot respond to the request.")
 	}
 
-	response.Status = "200 OK"
-	response.StatusCode = http.StatusOK
-	response.Header.Del("Location")
-	response.Header.Set("Content-Type", "application/json")
-	response.Body = ioutil.NopCloser(bytes.NewReader(body))
-	response.ContentLength = int64(len(body))
-	response.Header.Set("Content-Length", strconv.Itoa(len(body)))
+	r.Body = ioutil.NopCloser(bytes.NewReader(body))
+	r.ContentLength = int64(len(body))
+	r.StatusCode = http.StatusOK
+	r.Header = http.Header{}
+	r.Header.Set("Content-Type", "application/json")
+	r.Header.Set("Content-Length", strconv.Itoa(len(body)))
 }
