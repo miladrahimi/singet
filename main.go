1744e7be3d1aa6ee9b5d0f37622d77d5f374e160
Milad Rahimi <realmiladrahimi@gmail.com>
2022-04-28 19:40:54 +0430

    reformat the code

--- main.go
+++ main.go
@@ -1,140 +1,140 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-	"strconv"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+    "os"
+    "strconv"
 )
 
 const (
-	DefaultHost = "http://0.0.0.0"
-	DefaultPort = "8080"
+    DefaultHost = "http://0.0.0.0"
+    DefaultPort = "8080"
 )
 
 func main() {
-	proxyUrl := host() + ":" + port()
+    proxyUrl := host() + ":" + port()
 
-	http.HandleFunc("/favicon.ico", func(rw http.ResponseWriter, r *http.Request) {
-		favicon(rw)
-	})
+    http.HandleFunc("/favicon.ico", func(rw http.ResponseWriter, r *http.Request) {
+        favicon(rw)
+    })
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, request *http.Request) {
-		rw.Header().Set("Access-Control-Allow-Origin", "*")
+    http.HandleFunc("/", func(rw http.ResponseWriter, request *http.Request) {
+        rw.Header().Set("Access-Control-Allow-Origin", "*")
 
-		query := request.URL.Query()
+        query := request.URL.Query()
 
-		if query.Get("url") == "" {
-			displayError(rw, "Nothing requested.")
-			return
-		}
+        if query.Get("url") == "" {
+            displayError(rw, "Nothing requested.")
+            return
+        }
 
-		target, err := url.Parse(query.Get("url"))
-		if err != nil || target.IsAbs() == false {
-			displayError(rw, "URL is invalid.")
-			return
-		}
+        target, err := url.Parse(query.Get("url"))
+        if err != nil || target.IsAbs() == false {
+            displayError(rw, "URL is invalid.")
+            return
+        }
 
-		// Make a reverse proxy
-		proxy := &httputil.ReverseProxy{
-			Director: func(r *http.Request) {
-				for name, value := range request.URL.Query() {
-					if name[0:3] == "h__" {
-						r.Header.Del(name[3:])
+        // Make a reverse proxy
+        proxy := &httputil.ReverseProxy{
+            Director: func(r *http.Request) {
+                for name, value := range request.URL.Query() {
+                    if name[0:3] == "h__" {
+                        r.Header.Del(name[3:])
 
-						for _, v := range value {
-							r.Header.Set(name[3:], v)
-						}
-					}
-				}
+                        for _, v := range value {
+                            r.Header.Set(name[3:], v)
+                        }
+                    }
+                }
 
-				r.Host = target.Host
-				r.URL = target
+                r.Host = target.Host
+                r.URL = target
 
-				log.Println(r)
-			},
+                log.Println(r)
+            },
 
-			ModifyResponse: func(r *http.Response) error {
-				r.Header.Del("Access-Control-Allow-Origin")
+            ModifyResponse: func(r *http.Response) error {
+                r.Header.Del("Access-Control-Allow-Origin")
 
-				// Handle redirection responses
-				if r.StatusCode >= 300 && r.StatusCode < 400 && r.Header.Get("Location") != "" {
-					if query.Get("redirection") == "follow" {
-						r.Header.Set("Location", proxyUrl+"/?redirection=follow&url="+r.Header.Get("Location"))
-					} else if query.Get("redirection") == "stop" {
-						displayLocation(r, r.Header.Get("Location"))
-					}
-				}
+                // Handle redirection responses
+                if r.StatusCode >= 300 && r.StatusCode < 400 && r.Header.Get("Location") != "" {
+                    if query.Get("redirection") == "follow" {
+                        r.Header.Set("Location", proxyUrl+"/?redirection=follow&url="+r.Header.Get("Location"))
+                    } else if query.Get("redirection") == "stop" {
+                        displayLocation(r, r.Header.Get("Location"))
+                    }
+                }
 
-				return nil
-			},
-		}
+                return nil
+            },
+        }
 
-		proxy.ServeHTTP(rw, request)
-	})
+        proxy.ServeHTTP(rw, request)
+    })
 
-	log.Println("Serving on " + proxyUrl)
-	log.Fatal(http.ListenAndServe(":"+port(), nil))
+    log.Println("Serving on " + proxyUrl)
+    log.Fatal(http.ListenAndServe(":"+port(), nil))
 }
 
 // host will return the proxy host
 func host() string {
-	if host := os.Getenv("HOST"); host == "" {
-		return DefaultHost
-	} else {
-		return host
-	}
+    if host := os.Getenv("HOST"); host == "" {
+        return DefaultHost
+    } else {
+        return host
+    }
 }
 
 // port will return the proxy port
 func port() string {
-	if port := os.Getenv("PORT"); port == "" {
-		return DefaultPort
-	} else {
-		return port
-	}
+    if port := os.Getenv("PORT"); port == "" {
+        return DefaultPort
+    } else {
+        return port
+    }
 }
 
 // favicon will return the favicon icon
 func favicon(rw http.ResponseWriter) {
-	content, _ := ioutil.ReadFile("favicon.ico")
-	rw.Header().Set("Content-Type", "image/x-icon")
-	_, _ = fmt.Fprintln(rw, string(content))
+    content, _ := ioutil.ReadFile("favicon.ico")
+    rw.Header().Set("Content-Type", "image/x-icon")
+    _, _ = fmt.Fprintln(rw, string(content))
 }
 
 // displayError will create an error response
 func displayError(rw http.ResponseWriter, error string) {
-	rw.WriteHeader(http.StatusBadRequest)
-	rw.Header().Set("Content-type", "application/json")
+    rw.WriteHeader(http.StatusBadRequest)
+    rw.Header().Set("Content-type", "application/json")
 
-	body, err := json.Marshal(map[string]string{"error": error})
-	if err != nil {
-		panic("Cannot generate the JSON.")
-	}
+    body, err := json.Marshal(map[string]string{"error": error})
+    if err != nil {
+        panic("Cannot generate the JSON.")
+    }
 
-	_, err = rw.Write(body)
-	if err != nil {
-		panic("Cannot display the error.")
-	}
+    _, err = rw.Write(body)
+    if err != nil {
+        panic("Cannot display the error.")
+    }
 }
 
 // displayLocation will modify the response to a display location instead of redirecting
 func displayLocation(r *http.Response, location string) {
-	body, err := json.Marshal(map[string]string{"location": location})
-	if err != nil {
-		panic("Cannot display the location.")
-	}
+    body, err := json.Marshal(map[string]string{"location": location})
+    if err != nil {
+        panic("Cannot display the location.")
+    }
 
-	r.Body = ioutil.NopCloser(bytes.NewReader(body))
-	r.ContentLength = int64(len(body))
-	r.StatusCode = http.StatusOK
-	r.Header = http.Header{}
-	r.Header.Set("Content-Type", "application/json")
-	r.Header.Set("Content-Length", strconv.Itoa(len(body)))
+    r.Body = ioutil.NopCloser(bytes.NewReader(body))
+    r.ContentLength = int64(len(body))
+    r.StatusCode = http.StatusOK
+    r.Header = http.Header{}
+    r.Header.Set("Content-Type", "application/json")
+    r.Header.Set("Content-Length", strconv.Itoa(len(body)))
 }
